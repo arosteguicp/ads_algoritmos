b9a5cc191af3a14cb6524b2feee3106610a2304d
Alejandro <carlos.colfer@utec.edu.pe>
2023-08-23 15:00:33 -0500

    Test Case: n < k

---
+++ CountSortTest2.cpp
@@ -0,0 +1,28 @@
+#include "functions.h" 
+
+//Experimentacion de counting_sort n < k
+int main() {
+    srand(time(NULL)); // Inicializar semilla para generar valores aleatorios
+
+    std::vector<long int> data_sizes = {10, 100, 1000, 10000, 100000, 1000000};
+    std::vector<chrono::milliseconds> algorithm_times;
+
+    for (long int n : data_sizes) {
+        long int min_value = static_cast<long int>(n * 0.1);
+        long int max_value = static_cast<long int>(n - 1); 
+        std::vector<long int> arreglo = generar_arreglo<std::vector<long int>>(n, min_value , max_value);
+
+        auto start_time = std::chrono::high_resolution_clock::now();
+        countsort(arreglo);
+        auto end_time = std::chrono::high_resolution_clock::now();
+
+        auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time);
+        algorithm_times.push_back(duration);
+    }
+
+    for (int i = 0; i < data_sizes.size(); ++i) {
+        std::cout << "n = " << data_sizes[i] << ", Time: " << algorithm_times[i].count() << " ms" << std::endl;
+    }
+
+    return 0;
+}
