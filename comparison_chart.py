654ae3489de6747f61cc731b0b8e4e4f108ef6d7
noriaku <noriaku.cs@gmail.com>
2023-08-23 19:45:49 -0500

    fix(cc): comment bubble sort & focus on sorts also add labels

--- comparison_chart.py
+++ comparison_chart.py
@@ -9,15 +9,16 @@ countsort_times = df[df["sort"] == "count_sort"]["time"].tolist();
 radix_times = df[df["sort"] == "radix_sort"]["time"].tolist();
 sizes = df[df["sort"] == "radix_sort"]["size"].tolist();
 
-bubble_sizes = df[(df["sort"] == "bubble_sort") & (df["size"] <= 100000)]["size"].tolist()
-bubble_times = df[(df["sort"] == "bubble_sort") & (df["size"] <= 100000)]["time"].tolist()
+# --> for bubble sort
+# bubble_sizes = df[(df["sort"] == "bubble_sort") & (df["size"] <= 100000)]["size"].tolist()
+# bubble_times = df[(df["sort"] == "bubble_sort") & (df["size"] <= 100000)]["time"].tolist()
 
 # |====| plot section 
-plt.figure(figsize=(10, 6))
 
 plt.plot(sizes, countsort_times, label='Counting Sort', marker='*');
 plt.plot(sizes, radix_times, label='Radix Sort', marker='*');
-plt.plot(bubble_sizes, bubble_times, label='Bubble Sort', marker='o')
+# --> for bubble sort
+# plt.plot(bubble_sizes, bubble_times, label='Bubble Sort', marker='o')
 
 labels = ["0", "20k", "40k", "60k", "80k", "100k", "300k", "600k", "800k", "1M"]
 plt.xticks(sizes, labels, rotation=45)
@@ -28,4 +29,9 @@ plt.title('Grafico comparativo.')
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
+
+# --> for bubble sort
+# plt.xlim(0, 150000)  # Ajusta el límite del eje x para centrarte en el rango de 0 a 150k
+
+
 plt.show()
