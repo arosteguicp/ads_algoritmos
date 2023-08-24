aa0e8a5e2838e957fc6fcd975fb00acaefa06dfc
noriaku <noriaku.cs@gmail.com>
2023-08-23 19:31:20 -0500

    feat(comparison): add new scale & graphic

--- comparison_chart.py
+++ comparison_chart.py
@@ -7,17 +7,25 @@ df = pd.read_csv("tester_times.csv", header=None, names=["size", "sort", "time"]
 
 countsort_times = df[df["sort"] == "count_sort"]["time"].tolist();
 radix_times = df[df["sort"] == "radix_sort"]["time"].tolist();
-bubble_times = df[df["sort"] == "bubble_sort"]["time"].tolist();
 sizes = df[df["sort"] == "radix_sort"]["size"].tolist();
 
+bubble_sizes = df[(df["sort"] == "bubble_sort") & (df["size"] <= 100000)]["size"].tolist()
+bubble_times = df[(df["sort"] == "bubble_sort") & (df["size"] <= 100000)]["time"].tolist()
+
 # |====| plot section 
+plt.figure(figsize=(10, 6))
+
 plt.plot(sizes, countsort_times, label='Counting Sort', marker='*');
 plt.plot(sizes, radix_times, label='Radix Sort', marker='*');
-plt.plot(sizes, bubble_times, label='Bubble Sort', marker='*');
+plt.plot(bubble_sizes, bubble_times, label='Bubble Sort', marker='o')
+
+labels = ["0", "20k", "40k", "60k", "80k", "100k", "300k", "600k", "800k", "1M"]
+plt.xticks(sizes, labels, rotation=45)
 
 plt.xlabel('Cantidad de elementos')
 plt.ylabel('Tiempo (ms)')
 plt.title('Grafico comparativo.')
 plt.legend()
 plt.grid(True)
+plt.tight_layout()
 plt.show()
